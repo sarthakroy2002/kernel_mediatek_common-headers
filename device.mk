1cb31530270ed7345c1a585c3894c9635124e598
Gagan Malvi <malvigagan@gmail.com>
2021-06-08 05:05:06 +0200

    common: Copy common seccomp policies.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -2,4 +2,11 @@
 # Copyright (C) 2021 The Styx Project
 #
 
+LOCAL_PATH := $(MTK_ROOT_PATH)
+
+# SECCOMP Policy
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+     $(LOCAL_PATH)/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
+     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
