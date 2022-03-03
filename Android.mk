ab68522a4ba3b8f98a8ff8f15fc6d41c8124973a
Sarthak Roy <sarthakroy2002@gmail.com>
2022-03-03 22:09:51 +0530

    common: Switch to Kernel headers only
    Change-Id: Id63ff0a67d2afe11f2090faa32a4df1d4a8bf439

--- Android.mk
+++ Android.mk
@@ -1,8 +1,4 @@
-ifeq ($(TARGET_USES_MEDIATEK_COMMON),true)
-
 MTK_ROOT_PATH := $(call my-dir)
 
 # MTK Proprietary
 include $(call all-makefiles-under, $(MTK_ROOT_PATH))
-
-endif # TARGET_USES_MEDIATEK_COMMON
--- device.mk
+++ device.mk
@@ -2,20 +2,6 @@
 # Copyright (C) 2021 The Styx Project
 #
 
-LOCAL_PATH := device/mediatek/common
+LOCAL_PATH := kernel/mediatek/common-headers
 
 PRODUCT_VENDOR_KERNEL_HEADERS := $(LOCAL_PATH)/kernel-headers
-
-# SECCOMP Policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
-# APN configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-
-# priv-app permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
